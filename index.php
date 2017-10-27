5fdbe99103c43d0391cc78339f454c71e988fe35
Jesus Lopez <jalopezvillatoro@gmail.com>
2017-10-27 07:36:17 -0600

    ii

---
+++ index.php
@@ -0,0 +1,304 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <script src="https://use.fontawesome.com/5afdb6ace1.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="ieo-page">
+      <!-- cabecera -->
+      <header class="containter-fluid ieo-header fixed-top">
+        <nav class="container navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" href="#">
+           <img src="img/logo-inexon.png" height="30" alt="">
+         </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+            <form class="form-inline my-2 my-lg-0 ml-auto justify-content-center">
+              <input class="form-control mr-sm-2 ieo-input-text" type="search" placeholder="Que quieres aprender?" aria-label="Search">
+              <button class="ieo-btn --green my-2 my-sm-0" type="submit"><i class="fa fa-search" aria-hidden="true"></i> Buscar</button>
+            </form>
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <div class="dropdown-wrapper">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <!-- fiincabecera -->
+
+      <!-- Cover -->
+      <div class="container-fluid ieo-category">
+        <div class="container py-3">
+          <h2 class="ieo-title">Sobre que quieres aprender hoy?</h2>
+          <div class="row">
+
+            <div class="ieo-category-item col-12 col-sm-6 col-md-4 col-lg-3 my-sm-2">
+              <a href="#" class="ieo-category-link --purpel py-3 px-3">
+                <div class="ieo-category-img">
+                  <img class="" src="img/category/binaryoption.jpg" height="70">
+                </div>
+                <h3 class="ieo-category-title px-3">Opciones binarias</h3>
+              </a>
+            </div>
+
+            <div class="ieo-category-item col-12 col-sm-6 col-md-4 col-lg-3 my-sm-2">
+              <a href="#" class="ieo-category-link --aqua py-3 px-3">
+                <div class="ieo-category-img">
+                  <img class="" src="img/category/binaryoption.jpg" height="70">
+                </div>
+                <h3 class="ieo-category-title px-3">Invertir en Forex</h3>
+              </a>
+            </div>
+
+            <div class="ieo-category-item col-12 col-sm-6 col-md-4 col-lg-3 my-sm-2">
+              <a href="#" class="ieo-category-link --blue py-3 px-3">
+                <div class="ieo-category-img">
+                  <img class="" src="img/category/binaryoption.jpg" height="70">
+                </div>
+                <h3 class="ieo-category-title px-3">Tienda Online</h3>
+              </a>
+            </div>
+
+            <div class="ieo-category-item col-12 col-sm-6 col-md-4 col-lg-3 my-sm-2">
+              <a href="#" class="ieo-category-link --green py-3 px-3">
+                <div class="ieo-category-img">
+                  <img class="" src="img/category/binaryoption.jpg" height="70">
+                </div>
+                <h3 class="ieo-category-title px-3">Freelanzer</h3>
+              </a>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+      <!-- finCover -->
+
+
+      <section class="ieo-post container-fluid">
+        <div class="container">
+          <div class="row">
+
+
+            <div class="col-12  col-md-6 col-lg-4 my-3">
+              <div class="ieo-post-item ">
+                <a href="#" class="ieo-post-category">
+                  <h3 class="ieo-post-category-name">
+                    Opciones Binarias
+                  </h3>
+                  <span class="ieo-post-category-name ml-auto">
+                    12/12/19
+                  </span>
+                </a>
+                <a href="#" class="ieo-post-link">
+                  <div class="ieo-post-thumbnail">
+                    <img src="img/category/binaryoption.jpg" alt="">
+                  </div>
+                  <div class="ieo-post-copy text-justify">
+                    <h1 class="ieo-post-title">
+                      Como Operar con opciones
+                    </h1>
+                    <p class="ieo-post-text">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing e
+                      lit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    </p>
+                  </div>
+                </a>
+                <div class="ieo-post-footer">
+                  <a href="#" class="ieo-post-author">
+                    <img class="ieo-post-author-photo" src="https://randomuser.me/api/portraits/women/87.jpg" alt="">
+                    <span class="ieo-post-uthor-name">Alberto</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="col-12  col-md-6 col-lg-4 my-3">
+              <div class="ieo-post-item ">
+                <a href="#" class="ieo-post-category">
+                  <h3 class="ieo-post-category-name">
+                    Opciones Binarias
+                  </h3>
+                  <span class="ieo-post-category-name ml-auto">
+                    12/12/19
+                  </span>
+                </a>
+                <a href="#" class="ieo-post-link">
+                  <div class="ieo-post-thumbnail">
+                    <img src="img/category/binaryoption.jpg" alt="">
+                  </div>
+                  <div class="ieo-post-copy text-justify">
+                    <h1 class="ieo-post-title">
+                      Como Operar con opciones
+                    </h1>
+                    <p class="ieo-post-text">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing e
+                      lit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    </p>
+                  </div>
+                </a>
+                <div class="ieo-post-footer">
+                  <a href="#" class="ieo-post-author">
+                    <img class="ieo-post-author-photo" src="https://randomuser.me/api/portraits/women/87.jpg" alt="">
+                    <span class="ieo-post-uthor-name">Alberto</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="col-12  col-md-6 col-lg-4 my-3">
+              <div class="ieo-post-item ">
+                <a href="#" class="ieo-post-category">
+                  <h3 class="ieo-post-category-name">
+                    Opciones Binarias
+                  </h3>
+                  <span class="ieo-post-category-name ml-auto">
+                    12/12/19
+                  </span>
+                </a>
+                <a href="#" class="ieo-post-link">
+                  <div class="ieo-post-thumbnail">
+                    <img src="img/category/binaryoption.jpg" alt="">
+                  </div>
+                  <div class="ieo-post-copy text-justify">
+                    <h1 class="ieo-post-title">
+                      Como Operar con opciones
+                    </h1>
+                    <p class="ieo-post-text">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing e
+                      lit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    </p>
+                  </div>
+                </a>
+                <div class="ieo-post-footer">
+                  <a href="#" class="ieo-post-author">
+                    <img class="ieo-post-author-photo" src="https://randomuser.me/api/portraits/women/87.jpg" alt="">
+                    <span class="ieo-post-uthor-name">Alberto</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="col-12  col-md-6 col-lg-4 my-3">
+              <div class="ieo-post-item ">
+                <a href="#" class="ieo-post-category">
+                  <h3 class="ieo-post-category-name">
+                    Opciones Binarias
+                  </h3>
+                  <span class="ieo-post-category-name ml-auto">
+                    12/12/19
+                  </span>
+                </a>
+                <a href="#" class="ieo-post-link">
+                  <div class="ieo-post-thumbnail">
+                    <img src="img/category/binaryoption.jpg" alt="">
+                  </div>
+                  <div class="ieo-post-copy text-justify">
+                    <h1 class="ieo-post-title">
+                      Como Operar con opciones
+                    </h1>
+                    <p class="ieo-post-text">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing e
+                      lit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    </p>
+                  </div>
+                </a>
+                <div class="ieo-post-footer">
+                  <a href="#" class="ieo-post-author">
+                    <img class="ieo-post-author-photo" src="https://randomuser.me/api/portraits/women/87.jpg" alt="">
+                    <span class="ieo-post-uthor-name">Alberto</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="col-12  col-md-6 col-lg-4 my-3">
+              <div class="ieo-post-item ">
+                <a href="#" class="ieo-post-category">
+                  <h3 class="ieo-post-category-name">
+                    Opciones Binarias
+                  </h3>
+                  <span class="ieo-post-category-name ml-auto">
+                    12/12/19
+                  </span>
+                </a>
+                <a href="#" class="ieo-post-link">
+                  <div class="ieo-post-thumbnail">
+                    <img src="img/category/binaryoption.jpg" alt="">
+                  </div>
+                  <div class="ieo-post-copy text-justify">
+                    <h1 class="ieo-post-title">
+                      Como Operar con opciones
+                    </h1>
+                    <p class="ieo-post-text">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing e
+                      lit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    </p>
+                  </div>
+                </a>
+                <div class="ieo-post-footer">
+                  <a href="#" class="ieo-post-author">
+                    <img class="ieo-post-author-photo" src="https://randomuser.me/api/portraits/women/87.jpg" alt="">
+                    <span class="ieo-post-uthor-name">Alberto</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
